3b8dbf5878f67843c1ea0e880671a39c5a8acb8e
Die <dlibonati@leafnoise.io>
2024-11-09 12:31:40 -0300

    patch: router providers updated

--- flags-game-app/src/router/routes/PublicRoutes/PublicRoutes.tsx
+++ flags-game-app/src/router/routes/PublicRoutes/PublicRoutes.tsx
@@ -7,17 +7,61 @@ import { GamePage } from "../../../pages/GamePage/GamePage";
 import { StartGamePage } from "../../../pages/StartGamePage/StartGamePage";
 import { FinishGamePage } from "../../../pages/FinishGamePage/FinishGamePage";
 
+import { UsersProvider } from "../../../context/UsersContext/UsersProvider";
+import { ModesProvider } from "../../../context/ModesContext/ModesProvider";
+import { ModeProvider } from "../../../context/ModeContext/ModeProvider";
+import { FlagsProvider } from "../../../context/FlagsContext/FlagsProvider";
+import { GameProvider } from "../../../context/GameContext/GameProvider";
+
 export const PublicRoutes = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage></HomePage>}></Route>
-      <Route path="/menu" element={<MenuPage></MenuPage>}></Route>
-      <Route path="/menu/:mode" element={<MenuModePage></MenuModePage>}></Route>
-      <Route path="/menu/:mode/start" element={<StartGamePage></StartGamePage>}></Route>
-      <Route path="/menu/:mode/game" element={<GamePage></GamePage>}></Route>
       <Route
-        path="/menu/:mode/finishgame"
-        element={<FinishGamePage></FinishGamePage>}
+        path="/"
+        element={
+          <UsersProvider>
+            <HomePage></HomePage>
+          </UsersProvider>
+        }
+      ></Route>
+      <Route
+        path="/menu"
+        element={
+          <ModesProvider>
+            <MenuPage></MenuPage>
+          </ModesProvider>
+        }
+      ></Route>
+      <Route
+        path="/menu/:mode"
+        element={
+          <ModeProvider>
+            <UsersProvider>
+              <MenuModePage></MenuModePage>
+            </UsersProvider>
+          </ModeProvider>
+        }
+      ></Route>
+
+      <Route
+        path="/menu/:mode/*"
+        element={
+          <FlagsProvider>
+            <ModeProvider>
+              <GameProvider>
+                <Routes>
+                  <Route path="game" element={<GamePage />} />
+                  <Route path="finishgame" element={<FinishGamePage />} />
+                </Routes>
+              </GameProvider>
+            </ModeProvider>
+          </FlagsProvider>
+        }
+      ></Route>
+
+      <Route
+        path="/menu/:mode/start"
+        element={<StartGamePage></StartGamePage>}
       ></Route>
     </Routes>
   );
