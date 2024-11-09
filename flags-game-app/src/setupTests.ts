c286461f62040a8940ef9430f487a5d70cbcf791
Die <dlibonati@leafnoise.io>
2024-11-09 12:29:22 -0300

    patch: update config tests and new library added msw

--- flags-game-app/src/setupTests.ts
+++ flags-game-app/src/setupTests.ts
@@ -2,3 +2,5 @@ import "@testing-library/jest-dom";
 import { TextEncoder } from "util";
 
 global.TextEncoder = TextEncoder;
+global.TransformStream = global.TransformStream || function () {}; // Añadir esta línea
+global.BroadcastChannel = global.BroadcastChannel || function () {}; // Añadir esta línea
--- flags-game-app/src/test/server.ts
+++ flags-game-app/src/test/server.ts
@@ -4,14 +4,17 @@ import { http, HttpResponse } from "msw";
 
 type HandleConfig = {
   path: string;
-  method: "get" | "post" | "delete" | "patch";
+  method: "get" | "post" | "delete" | "patch" | "put";
+  status?: number;
   res: () => Record<string, unknown> | Record<string, unknown>[];
 };
 
 export const createServer = (handlerConfig: HandleConfig[]): void => {
   const handlers: RequestHandler[] = handlerConfig.map((handleConfig) => {
     return http[handleConfig.method](handleConfig.path, () => {
-      return HttpResponse.json(handleConfig.res());
+      return HttpResponse.json(handleConfig.res(), {
+        status: handleConfig.status ?? 200,
+      });
     });
   });
 
